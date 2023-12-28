df60077ca71e7399f1f885a923df50c9a4509cec
Lars Smith Eier <lars.smith.eier@basefarm-orange.com>
2023-12-28 21:01:09 +0100

    testing

--- flake.nix
+++ flake.nix
@@ -24,6 +24,8 @@
   in
   {
     nixosConfigurations = {
+      inherit system;
+      specialArgs = inputs;
 
       desktop = nixpkgs.lib.nixosSystem {
         inherit system;
@@ -50,8 +52,6 @@
       };
 
       testing_vm = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = inputs;
         modules = [
           ./hosts/testing_vm/configuration.nix
         ];
