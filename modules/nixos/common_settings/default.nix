06c70681e4e2ad95e763ceb65a68ad82dc43e137
Lars Smith Eier <lars.smith.eier@basefarm-orange.com>
2023-12-29 01:35:47 +0100

    asdasd

--- modules/nixos/common_settings/default.nix
+++ modules/nixos/common_settings/default.nix
@@ -21,7 +21,7 @@
   nixpkgs.overlays = [
     (final: prev: {
       unstable = import nixpkgs-unstable {
-        system = "${config.system}";
+        system = config.system;
         config = config.nixpkgs.config;
       };
     })
